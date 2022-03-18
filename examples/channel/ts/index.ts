8de8c12905f194245fe2de6e708c838bcadd9051
Jamie Kinkead <jamie@pulumi.com>
2022-03-18 12:20:08 -0700

    update test

--- examples/channel/ts/index.ts
+++ examples/channel/ts/index.ts
@@ -1,9 +1,9 @@
 import * as pulumi from "@pulumi/pulumi";
-import { Conversation } from "@pulumi/slack";
+import * as slack from "@pulumi/slack";
 
 // Create a new Slack channel
-const channel = new Conversation("platychat", {
-  name: "platychat",
+const channel = new Conversation("test", {
+  name: "test-chat",
   topic: "my topic",
   isPrivate: false,
 });
