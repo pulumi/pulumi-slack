6c0a84cd136feba9d6dc9bf83a2cc1d1a2b6cc0c
Jamie Kinkead <41454626+jkisk@users.noreply.github.com>
2022-03-21 11:41:13 -0700

    fix node test (#2)
    Co-authored-by: stack72 <public@paulstack.co.uk>

--- examples/channel/ts/index.ts
+++ examples/channel/ts/index.ts
@@ -1,12 +1,11 @@
 import * as pulumi from "@pulumi/pulumi";
-import { SlackConversation } from "@pulumi/slack"
+import * as slack from "@pulumi/slack";
 
 // Create a new Slack channel
-const channel = new SlackConversation("platychat", {
-    name: "platychat",
-    topic: "my topic",
-    isPrivate: false,
-})
+const channel = new slack.Conversation("acc-test", {
+  topic: "my topic",
+  isPrivate: false,
+});
 
 // Get the id of the new channel as an output
-export const channelId = channel.id
+export const channelId = channel.id;
