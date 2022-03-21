b5d140de3be934aed23a539ff1d81e64fb65c351
stack72 <public@paulstack.co.uk>
2022-03-21 18:26:18 +0000

    Ensure we use a random name on the channel

--- examples/channel/ts/index.ts
+++ examples/channel/ts/index.ts
@@ -2,8 +2,7 @@ import * as pulumi from "@pulumi/pulumi";
 import * as slack from "@pulumi/slack";
 
 // Create a new Slack channel
-const channel = new slack.Conversation("test", {
-  name: "test-chat",
+const channel = new slack.Conversation("acc-test", {
   topic: "my topic",
   isPrivate: false,
 });
